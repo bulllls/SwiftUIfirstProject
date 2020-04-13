fb68fb0035358c846ba77febebb1501c38f602a7
Ruslan <ruslanfilistovich@gmail.com>
2020-04-13 14:36:25 +0300

    Initial Commit

---
+++ CourseListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CourseListApp
+//
+//  Created by Ruslan on 4/13/20.
+//  Copyright © 2020 Ruslan Filistovich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
